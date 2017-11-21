37f86bd39d02c211067ed6f09a88cbff0fd41809
Free-loops <1696905265@qq.com>
2017-11-21 23:51:32 +0800

    2017/11/21

--- project/08-乐淘云购流式布局/js/index.js
+++ project/08-乐淘云购流式布局/js/index.js
@@ -1,110 +1,43 @@
+var lt_banner = document.getElementById('lt_banner');
 var banner = document.getElementById('banner');
-var btn = document.getElementById('btn');
+var btnParent = document.getElementById('btn');
+var btns = btnParent.querySelectorAll('li');
+var width = lt_banner.offsetWidth;
 
-var scale = -12.5;
 var index = 0;
 var timer = setInterval(function(){
-    index++;
-    scale-=12.5;
-    if(index==6){
-        animate(banner,scale);
-        //如果运动到了最后一张(最后一个li) 再迅速显示为第一张(第二个li)
-        scale=-12.5;//第二个li的位置
-        banner.style.transform = "translateX("+scale+"%)";
-        index=0;//并将index重置到0
-    }else{
-        animate(banner,scale);
-    }
-    //六个按钮
-    for (var i = 0; i < btn.children.length; i++) {
-        btn.children[i].classList.remove('active')
-    }
-    btn.children[index].classList.add('active');
-},3000)
-//运动函数封装
-function animate(obj,scale){
-    var a = scale+12.5;
-    var timer = setInterval(function(){
-        a-=0.3;
-        if(a<=scale){
-            a=scale;
-            clearInterval(timer);
-            timer=0;
-        }
-        obj.style.transform = "translateX("+a+"%)";
-    },10)
+    index ++;
+    addTransition();
+    addTransform(-width*index);
+},2000)
+
+var addTransition = function () {
+    banner.style.transition = 'all 0.3s';
+    banner.style.webkitTransition = 'all 0.3s';
+}
+var removeTransition = function () {
+    banner.style.transition = 'none';
+    banner.style.webkitTransition = 'none';
+}
+var addTransform = function (x) {
+    banner.style.transform = 'translateX('+x+'px)';
+    banner.style.webkitTransform = 'translateX('+x+'px)';    
 }
 
-
-var startX, startY, moveEndX, moveEndY, X, Y;   
-
-banner.addEventListener('touchstart', function(e) {
-
-    e.preventDefault();//阻止滑动滚动行为
-
-    startX = e.touches[0].pageX;
-
-    startY = e.touches[0].pageY;
-
-});
-
-banner.addEventListener('touchmove', function(e) {
-
-    e.preventDefault();
-
-    moveEndX = e.changedTouches[0].pageX;
-
-    moveEndY = e.changedTouches[0].pageY;
-
-    X = moveEndX - startX;
-
-    Y = moveEndY - startY;
-
-    if ( X > 0 ) {
-        //向右
-        index--;
-        scale+=12.5;
-        if(index==-1){
-            animate(banner,scale);
-            //如果运动到了第一张(第一个li) 再迅速显示为最后一张(倒数第二个li)
-            scale=-75;//倒数第二个li的位置
-            banner.style.transform = "translateX("+scale+"%)";
-            index=5;//并将index重置到5
-        }else{
-            animate(banner,scale);
-        }
-        //六个按钮
-        for (var i = 0; i < btn.children.length; i++) {
-            btn.children[i].classList.remove('active')
-        }
-        btn.children[index].classList.add('active');
+banner.addEventListener('transitionend',function(){
+    if(index == 7) {
+        index = 1;
+        removeTransition();
+        addTransform(-width*index);
     }
-
-    else if ( X < 0 ) {
-        //向左
-        index++;
-        scale-=12.5;
-        if(index==6){
-            animate(banner,scale);
-            //如果运动到了最后一张(最后一个li) 再迅速显示为第一张(第二个li)
-            scale=-12.5;//第二个li的位置
-            banner.style.transform = "translateX("+scale+"%)";
-            index=0;//并将index重置到0
-        }else{
-            animate(banner,scale);
-        }
-        //六个按钮
-        for (var i = 0; i < btn.children.length; i++) {
-            btn.children[i].classList.remove('active')
-        }
-        btn.children[index].classList.add('active');
+    if(index == 0) {
+        index = 6;
+        removeTransition();
+        addTransform(-width*index);
     }
+    for (var i = 0; i < btns.length; i++) {
+        btns[i].classList.remove('active');
+    }
+    btns[index-1].classList.add('active');
+})
 
-    // else if ( Y > 0) {alert('向下');}
-
-    // else if ( Y < 0 ) { alert('向上');}
-
-    // else{alert('没滑动'); }
-
-});
-//动画积攒问题
